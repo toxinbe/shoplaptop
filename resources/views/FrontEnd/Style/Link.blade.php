591bdee305868f37ddaadec799b9ed948d277916
ToxinBe <toxinbe1809@gmail.com>
2025-01-18 23:24:23 +0700

    fix : base tag html

--- resources/views/FrontEnd/Style/Link.blade.php
+++ resources/views/FrontEnd/Style/Link.blade.php
@@ -8,7 +8,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <base href="{{ asset('') }}">
+    <!-- <base href="{{ asset('') }}"> -->
     <link href="{{ asset('source/assets/dest/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicons -->
